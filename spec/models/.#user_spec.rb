f613c6e166ef14b7014cac17ab2ac34c72e6d699
Derrick Mar <derrickmar1215@berkeley.edu>
2014-01-05 09:58:07 -0800

    Finish Chapter 7 exercises

---
+++ spec/models/.#user_spec.rb
@@ -0,0 +1,1 @@
+derrickmar1215@ThinkPad.6884:1388212448
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,29 +15,47 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 
-  describe "signup page" do
+  describe "signup" do
+
     before { visit signup_path }
+
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       it "should not create a user" do
-        initial = User.count
-        click_button "Create my account"
-        final = User.count
-        expect(initial).to eq final
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content('Password digest') }
       end
     end
 
-    describe "with valid information"
-    before do
-      fill_in "Name", with: "Example User"
-      fill_in "Email", with: "user@example.com"
-      fill_in "Password", with: "foobar"
-      fill_in "Confirmation", with: "foobar"
-    end
+    describe "with valid information" do
 
-    it "should create a user" do
-      expect { click_button submit }.to change(User, :count).by(1)
-    end
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving a user" do
+        before { click_button submit }
+
+        let(:user) { User.find_by_email("user@example.com") }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end #ending the describe "signup" do
   end
 end
