1d86ff7f27f3a43a36be3c7b3fb317b7652d2c55
Derrick Mar <derrickmar1215@berkeley.edu>
2014-01-07 07:15:54 -0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,10 +26,61 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # we don't need to have @user = User.find(params[:id]) anymore
+    # because the method correct_user handles it for us. Remember
+    # correct_user method is called first because of the
+    # before_action.
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+
+  # all methods below this a private
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # Before filters
+  #this redirects a user to signin_url if not signed in
+  def signed_in_user
+    unless signed_in?
+      store_location
+      flash[:notice] = "Please sign in."
+      redirect_to signin_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    # unless the user equals the current user (which is identified by
+    # the cookie and token) then redirect to the root_url. This is
+    # really only implemented for malicious intent because a user that
+    # is signed in is writing a link that like ~/users/3/edit and
+    # trying to edit another users information
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
---
+++ spec/.#factories.rb
@@ -0,0 +1,1 @@
+derrickmar1215@ThinkPad.27582:1388967442
